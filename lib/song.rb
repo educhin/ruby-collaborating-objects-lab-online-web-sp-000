99bb97df5864b7c0df8997c5904691d324dc6e37
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-16 02:24:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,13 +4,11 @@ class Song
     @title = title
   end
 
-  def self.new_by_filename(files)
-    files.collect do |file|
+  def self.new_by_filename(file)
       data = file.split(" - ")
       name = data[1].split(".")[0]
       song = Song.new(name)
 
       song
-    end
   end
 end
